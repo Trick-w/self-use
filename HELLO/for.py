4dc50bc7ce8193dc14f7098bab2717bf6811365b
Trick-w <1484797921@qq.com>
2024-11-30 16:38:39 +0800

    11.30

---
+++ HELLO/for.py
@@ -0,0 +1,10 @@
+pizzas = ['jobo','davie','casd']
+for pizza in pizzas:
+    print('I like '+pizza+'pizza')
+print('I really love pizza!')
+
+
+dongwus = ['dog','cats','niao','pig']
+for dongwu in dongwus:
+    print('A '+dongwu+' would make a greatpet')
+print('Any of these animals would make a great pet!')
