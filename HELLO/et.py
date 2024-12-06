008c2668c86bcbacba291e5d7001fa0d7d22b06f
Trick-w <1484797921@qq.com>
2024-12-06 13:52:27 +0800

    12.6

---
+++ HELLO/et.py
@@ -0,0 +1,21 @@
+age = 24
+if age<2:
+    print('牛马人生欢迎你的到来')
+elif age>=2 and age<4:
+    print('你即将步入牛马人生')
+elif age>=4 and age<13:
+    print('在你步入牛马人生前最后的快乐')
+elif age>=13 and age<20:
+    print('你在学习如何成为一个合格的牛马')
+elif age>=20 and age<65:
+    print('你已经是一个成熟的牛马了')
+else:
+    print('恭喜你,你的牛马人生即将结束')
+
+favorite_fruiits = 'banana','apple','li'
+if 'banana' in favorite_fruiits:
+    print('you really like banana.')
+if 'juzi' in favorite_fruiits:
+    print('you really like juzi.')
+if 'li' in favorite_fruiits:
+    print('you really like li.')
--- HELLO/zizhu.py
+++
@@ -1,7 +0,0 @@
-foods = ('xia','yu','rou')
-for food in foods:
-    print(food)
-foods = ('ren','yu','mei')
-for food in foods:
-    print(food)
-print(foods)
