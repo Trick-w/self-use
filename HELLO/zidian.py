0aede23375e218c70edc2ff55b023014d2e89b21
Trick-w <1484797921@qq.com>
2024-12-11 13:42:47 +0800

    1

---
+++ HELLO/zidian.py
@@ -0,0 +1,18 @@
+xinxi_1 = {
+    'mingzi':'fangyuan',
+    'shenfen':'大梦仙尊',
+    'age':18,
+    }
+xinxi_2 = {
+    'mingzi':'momnkey',
+    'shenfen':'qitaindasheng',
+    'age':500,
+    }
+xinxi_3 = {
+    'mingzi':'yangjian',
+    'shenfen':'erlangshen',
+    'age':12800
+    }
+peoples = [xinxi_1,xinxi_2,xinxi_3]
+for people in peoples:
+    print(people)
