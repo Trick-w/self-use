de76e585d3126cb1e3f18e598091b3dd4d0e2a9e
Trick-w <1484797921@qq.com>
2024-12-23 07:30:11 +0800

    为用户类添加文档字符串，描述用户信息和问候方法

--- user.py
+++ user.py
@@ -1,16 +1,17 @@
 class User():
+    """模拟用户信息"""
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
-
+    """描述用户信息"""
     def describe_user(self):
         print(f"First Name: {self.first_name}")
         print(f"Last Name: {self.last_name}")
         print(f"Email: {self.email}")
         print(f"Password: {self.password}")
-
+    """问候用户"""
     def greet_user(self):
         print(f"Hello, {self.first_name} {self.last_name}!")
 
